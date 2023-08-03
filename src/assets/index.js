9c2d2ceb7216014058dca8199c32387a598768cb
maria <winterzaq1@gmail.com>
2023-08-03 23:42:35 +0900

    working on navbar animations

--- src/assets/index.js
+++ src/assets/index.js
@@ -4,8 +4,8 @@ import creator from "./creator.png";
 import mobile from "./mobile.png";
 import web from "./web.png";
 import github from "./github.png";
-import menu from "./menu.svg";
-import close from "./close.svg";
+import menu from "./menu.png";
+import close from "./close.png";
 
 import css from "./tech/css.png";
 import docker from "./tech/docker.png";
@@ -29,6 +29,9 @@ import tesla from "./company/tesla.png";
 import carrent from "./carrent.png";
 import jobit from "./jobit.png";
 import tripguide from "./tripguide.png";
+
+import hovernav from "./hover_nav.json";
+import activenav from "./active_nav.json";
 
 export {
   logo,
@@ -59,4 +62,6 @@ export {
   carrent,
   jobit,
   tripguide,
+  hovernav,
+  activenav,
 };
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,10 +2,11 @@ import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { styles } from '../styles';
 import { navLinks } from '../constants';
-import { logo, menu, close } from '../assets';
+import { logo, menu, close, hovernav, activenav } from '../assets';
 
 const Navbar = () => {
 	const[active, setActive] = useState("")
+	const[toggle, setToggle] = useState(false);
 
   return (
     <nav className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}>
@@ -18,18 +19,36 @@ const Navbar = () => {
 					window.scrollTo(0, 0)
 				}}
 			>
-				<img src={logo} alt="logo" className="w-9 h-9 obuject-contain"/>
+				<img src={logo} alt="logo" draggable="false" className="w-9 h-9 object-contain"/>
 				<p className="text-title-pink text-[18px] font-bold cursor-pointer">maria yoshikawa
 					<span className="sm:block hidden">よしかわ まりあ</span>
 				</p>
 			</Link>
 			<ul className="list-none hidden sm:flex flex-row gap-10">
 				{navLinks.map((link) => (
-					<li key={link.id} className={link.title}>
+					<li key={link.id} className={`${
+						active === link.title 
+						? "text-white" 
+						: "text-secondary"}
+						hover:text-black text-[18px]
+						font-medium cursor-pointer`}
+						onClick={() => setActive(link.title)}>
 						<a href={`${link.id}`}>{link.title}</a>
 					</li>
 				))}
 			</ul>
+			<div className="sm:hidden flex flex-1 justify-end items-center">
+				<img
+					draggable="false"
+					src={toggle ? close : menu}
+					alt="menu" 
+					className="w-[28px] h-[28px] object-contain cursor-pointer"
+					onClick={() => setToggle(!toggle)}
+				/>
+				<div className={`${!toggle ? 'hidden' : 'flex'} 
+					p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10rounded-xl`}>
+				</div>
+			</div>
 		</div>
 	</nav>
   )
